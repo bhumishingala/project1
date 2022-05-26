9f053b5d2a31951aeb95cb3229df59d4f4c41837
bhumishingala <shingalabhumi28@gmail.com>
2022-05-26 15:06:26 +0530

    hii bhumi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by bhumi shingala
+          Learn React of asdghhfed
         </a>
       </header>
     </div>
